df25ddba865da9f35fa196cf7aade3b78a6fbb0f
FelipeTofollo <felipetofollo11@gmail.com>
2023-09-19 09:13:33 -0300

    configuração de inputs nos campos do formulario

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -1,41 +1,61 @@
 "use client";
-import { TextField } from "@mui/material";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import React from "react";
+import { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import TextInput from "../inputs/TextInput";
+import CpfInput from "../inputs/CpfInput";
+import DateInput from "../inputs/DateInput";
+import PhoneInput from "../inputs/PhoneInput";
 
-type Inputs = {
-  example: string;
-  exampleRequired: string;
+export type Inputs = {
+  cpf: string;
+  nome: string;
+  matricula: string;
+  data: string;
+  phone: string;
+  email: string;
 };
 
 export default function Register() {
   const {
     register,
-    control,
     handleSubmit,
+    control,
     watch,
     formState: { errors },
   } = useForm<Inputs>();
+
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data);
   };
 
+  console.log(errors);
+
+  console.log(watch("cpf"));
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="cpf"
+      <CpfInput control={control} label="CPF" name="cpf" errors={errors} />
+      <DateInput
         control={control}
-        rules={{ required: true }}
-        render={({ field }) => (
-          <TextField
-            error={errors.cpf ? true : false}
-            label="CPF"
-            defaultValue=""
-            helperText="CPF INVALIDO!"
-            {...field}
-          />
-        )}
+        label="Data de nascimento"
+        name="data"
+        errors={errors}
       />
-      <button type="submit">enviar</button>
+      <TextInput
+        control={control}
+        name="matricula"
+        label="Matrícula"
+        errors={errors}
+      />
+      <PhoneInput
+        control={control}
+        name="phone"
+        label="Telefone"
+        errors={errors}
+      />
+
+      <button type="submit">Enviar</button>
     </form>
   );
 }
